0e6e994db5276ca31c1af5b419518960783293e5
boiu.lebedev <lebedeff.boris@gmail.com>
2023-09-21 14:45:46 +0300

    Рефакторинг plaintext.py

--- STC/gui/windows/ancestors/plaintext.py
+++ STC/gui/windows/ancestors/plaintext.py
@@ -1,22 +1,31 @@
+""" Переопределение Qt класса QPlainTextEdit """
+
 from PyQt5.QtWidgets import QSizePolicy
 from PyQt5.QtWidgets import QPlainTextEdit
 
 
 class PlainTextResizable(QPlainTextEdit):
+    """ QPlainTextEdit с предустановками геометрии и
+        изменением размеров при печати текста в нем"""
+
     def __init__(self) -> None:
         super().__init__()
         self.initSettings()
         self.textChanged.connect(self.resizeSentence)
 
     def initSettings(self) -> None:
+        """ Настройки геометрии виджета """
+
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.MinimumExpanding)
         self.min_height = 20
         self.current_height = 0
-        self.fontSize = self.document().defaultFont().pointSize()
-        self.min_height = self.fontSize
+        self.font_size = self.document().defaultFont().pointSize()
+        self.min_height = self.font_size
 
     def resizeSentence(self) -> None:
+        """ Изменение размера при вписывании текста """
+
         self.setFixedHeight(self.min_height)
         self.setFixedHeight(self.height() + 1)
         while self.verticalScrollBar().isVisible() and self.height() < 1000:
-            self.setFixedHeight(self.height() + self.fontSize)
+            self.setFixedHeight(self.height() + self.font_size)
