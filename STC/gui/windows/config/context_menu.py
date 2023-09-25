df65aa002d262c2a65651a15e358e7ea48ef3cda
boiu.lebedev <lebedeff.boris@gmail.com>
2023-09-25 12:49:51 +0300

    Рефакторинг context_menu.py

--- STC/gui/windows/config/context_menu.py
+++ STC/gui/windows/config/context_menu.py
@@ -1,14 +1,21 @@
+""" Контекстные меню для окон администрирования БД """
+
 from PyQt5.QtWidgets import QAction
 from STC.gui.windows.ancestors.context_menu import ContextMenuForBasicTable
 
+
 class ContextMenuForFrameAdminDef(ContextMenuForBasicTable):
+    """ Родительский класс для контекстного меню таблицы
+        администрирования данных """
 
     def __init__(self, obj):
-        super(ContextMenuForFrameAdminDef, self).__init__(obj)
+        super().__init__(obj)
         self.addAction(self.copyRow())
 
     def copyRow(self) -> QAction:
+        """ Опция копирования строки """
+
         action = QAction(self.object)
         action.setText("Копировать строку")
-        action.triggered.connect(lambda: self.object.copyRow())
+        action.triggered.connect(self.object.copyRow)
         return action
