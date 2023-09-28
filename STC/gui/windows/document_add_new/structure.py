725f486f5a16f2e31b74c70879e0163fdd05808b
boiu.lebedev <lebedeff.boris@gmail.com>
2023-09-28 16:03:43 +0300

    Рефакторинг structure.py (Модуль структур окна ввода документов)

--- STC/gui/windows/document_add_new/structure.py
+++ STC/gui/windows/document_add_new/structure.py
@@ -1,3 +1,4 @@
+""" Модуль структур окна ввода документов """
 
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QGridLayout
@@ -10,16 +11,20 @@ from STC.gui.windows.document_add_new.frame import NewDocumentSpecProductsNoDeno
 from STC.gui.windows.document_add_new.frame import FrameComplexDocument
 
 
-# Структура окна меню внесения реквизитов документа
 class StructureNewDocument(StructureSideMenu):
+    """ Структура окна меню внесения реквизитов документа """
 
     def __init__(self, main_layout: QGridLayout) -> None:
         super().__init__(main_layout)
         self.showFrame(frame_name='Основные данные')
 
     def initNewDocumentFrames(self) -> None:
+        """ Рамки, которые добавляются в self.data_frame и
+            между которыми нужно переключаться с помощью
+            кнопок в рамке self.menu_frame """
+
         self.main_data = NewDocumentMainFrame()
-        self.main_data.findDocument.connect(lambda: self.widgetVisibility())
+        self.main_data.findDocument.connect(self.widgetVisibility)
         self.td_complex = FrameComplexDocument()
         self.spec_products = NewDocumentSpecProductsWithDeno()
         self.spec_products_no_deno = NewDocumentSpecProductsNoDeno()
@@ -31,6 +36,9 @@ class StructureNewDocument(StructureSideMenu):
                        self.td_complex]
 
     def widgetVisibility(self) -> None:
+        """ Видимость опций бокового меню в
+            зависимости от вида документа """
+
         if self.main_data.document_class == 'КД':
             if self.main_data.document_subtype == 'Спецификация':
                 self.widgetVisibilitySpec(True)
@@ -42,6 +50,8 @@ class StructureNewDocument(StructureSideMenu):
             self.widgetVisibilitySpec(False)
 
     def widgetVisibilitySpec(self, visibility: bool) -> None:
+        """ Изменяет видимость виджетов в зависимости от вида документа """
+
         self.changeButtonVisibility(button_name='Изделия с ДН',
                                     visibility=visibility)
         self.changeButtonVisibility(button_name='Изделия без ДН',
@@ -52,6 +62,8 @@ class StructureNewDocument(StructureSideMenu):
                                     visibility=not visibility)
 
     def changeButtonVisibility(self, button_name: str, visibility: bool) -> None:
+        """ Изменяет видимость виджетов в зависимости от вида документа """
+
         for widget in self.menu_frame.children():
             if isinstance(widget, QPushButton):
                 if widget.text() == button_name:
