fc1bfa48b48e974b215d66f07df5196f8fa7e9c8
boiu.lebedev <lebedeff.boris@gmail.com>
2023-10-06 11:31:19 +0300

    Рефакторинг model.py (окно фильтрации данных)

--- STC/gui/windows/hierarchy_filter/model.py
+++ STC/gui/windows/hierarchy_filter/model.py
@@ -1,14 +1,17 @@
+""" Модель данных для окна фильтрации данных """
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from STC.gui.windows.ancestors.model import StandartModel
+
 if TYPE_CHECKING:
     from STC.gui.windows.hierarchy.model import HierarchicalView
 
-from STC.gui.windows.ancestors.model import StandartModel
-
 
 class StandartModelFilter(StandartModel):
+    """ Модель данных для окна фильтрации данных """
 
     def __init__(self, tree_view: HierarchicalView) -> None:
         super().__init__(tree_view)
@@ -16,6 +19,8 @@ class StandartModelFilter(StandartModel):
         self.addData()
 
     def getData(self, column: int) -> list[str]:
+        """ Возвращает список уникальных значений для столбца """
+
         data = []
         for row in range(self.rowCount()):
             data.append(self.item(row, column).text())
